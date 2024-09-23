8f80e17e7f2bee83c0fa17ecac7aededf5a1cf92
Yo Kamada <bumpfuji10@gmail.com>
2024-09-23 22:02:34 +0900

    [add]aboutページを実装

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid">
+    <div class="mx-auto">
+      <img src="/public/img/me_image.png" alt="" class="rounded-full max-h-48 w-auto border-4 border-black bg-gray-400">
+    </div>
+    <div class="flex justify-center text-left">
+      <pre>
+        {
+          "name": "Yo Kamada",
+          "age": 28,
+          "hometown": "Shimane",
+          "liveIn": "Yokohama",
+          "job": "Web Engineer",
+          "jobSkills": {
+            "languages": ["Ruby", "JavaScript"],
+            "frameworks": ["Ruby on Rails", "Vue.js"],
+            "others": ["Git", "SQL", "Docker"]
+          },
+          "experienced": {
+            "languages": ["PHP", "TypeScript"],
+            "frameworks": ["Laravel"],
+            "others": ["GraphQL"]
+          },
+          "hobby": "Gymnastics"
+        }
+      </pre>
+    </div>
+  </div>
+</template>
