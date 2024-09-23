232999782869a93aeef30e1d8a233de716b95f02
Yo Kamada <bumpfuji10@gmail.com>
2024-09-23 20:31:35 +0900

    [add, change]homeやarticleShowのレイアウトを調整

--- pages/home.vue
+++ pages/home.vue
@@ -1,15 +1,23 @@
 <template>
-  <div v-for="(article, index) in articles" :key="article.id">
-    <h1 class="articleTitle">{{ article.title }}</h1>
+  <div class="mx-4">
+    <div v-for="(article, index) in articles" :key="index">
+      <div class="py-8 break-words" style="width: 100%;">
+        <nuxt-link :to="`/articles/${article.id}`">
+          <h1 class="mx-auto text-lg font-bold border-4 border-black rounded-lg py-4 bg-white hover:bg-gray-200">
+          {{ article.title }}
+          </h1>
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { getArticles } from '../src/api/article';
+import { defineComponent } from 'vue'
+import { getArticles } from '../src/api/article'
 
 interface Article {
-  id: number,
+  id: string,
   title: string,
   content: string,
   createdAt: ""
@@ -18,7 +26,7 @@ interface Article {
 export default defineComponent({
   data() {
     return {
-      articles: [] as Article[] // 配列として定義
+      articles: [] as Article[]
     }
   },
   created() {
@@ -28,8 +36,7 @@ export default defineComponent({
     async getArticles() {
       try {
         const response = await getArticles();
-        this.articles = response.data.contents; // レスポンスのデータを配列に代入
-        console.log(this.articles)
+        this.articles = response.data.contents;
       } catch (error) {
         console.error('記事の取得中にエラーが発生しました:', error);
       }
