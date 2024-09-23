f4deff384781714d4d1d509f424c1e4ddf829a96
Yo Kamada <bumpfuji10@gmail.com>
2024-09-23 20:31:46 +0900

    [add]暫定でheaderを追加

---
+++ components/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header>
+      <nuxt-link to="/">
+        <img src="/public/img/sphere.png" alt="" style="width: 32px">
+      </nuxt-link>
+      <nav>about</nav>
+    </header>
+  </div>
+</template>
