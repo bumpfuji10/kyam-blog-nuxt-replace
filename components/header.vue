4d6d189f4974a8c081714c6ca1d762784aebe7ad
Yo Kamada <bumpfuji10@gmail.com>
2024-09-23 22:02:19 +0900

    [add]headerにaboutへの導線を追加

--- components/header.vue
+++ components/header.vue
@@ -4,7 +4,9 @@
       <nuxt-link to="/">
         <img src="/public/img/sphere.png" alt="" style="width: 32px">
       </nuxt-link>
-      <nav>about</nav>
+      <nuxt-link to="/about">
+        about
+      </nuxt-link>
     </header>
   </div>
 </template>
