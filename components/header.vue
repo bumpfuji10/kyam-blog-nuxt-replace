092257a8d9bff2487aadf984016a963439081411
Yo Kamada <bumpfuji10@gmail.com>
2024-11-04 17:58:05 +0900

    [improve] header ui

--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <header>
-      <nuxt-link to="/">
-        <img src="/public/img/sphere.png" alt="" style="width: 32px">
-      </nuxt-link>
-      <nuxt-link to="/about">
-        about
-      </nuxt-link>
+    <header class="py-4">
+      <nav class="flex gap-4">
+        <nuxt-link to="/">
+          <img src="/public/img/sphere.png" alt="ブログのロゴ" style="width: 32px">
+        </nuxt-link>
+
+        <nuxt-link to="/" class="text-xl font-bold">
+          Kyam Blog
+        </nuxt-link>
+        <nuxt-link to="/about" class="text-xl font-bold">
+          about
+        </nuxt-link>
+      </nav>
     </header>
   </div>
 </template>
