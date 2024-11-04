4ffa8ce7b536f6d6caaeb32faae2153c3a098b85
Yo Kamada <bumpfuji10@gmail.com>
2024-11-04 18:27:33 +0900

    fix

--- pages/home.vue
+++ pages/home.vue
@@ -2,16 +2,14 @@
   <div class="mx-4">
     <div v-for="(article, index) in articles" :key="index">
       <div class="break-words article p-4" style="width: 100%;">
-        <div class="articleCard flex border-4 border-black rounded-lg p-4 bg-white hover:bg-gray-200 justify-between items-center">
-          <nuxt-link :to="`/articles/${article.id}`">
-            <h1 class="mx-auto text-lg font-bold break-keep" style="width: 190px;">
+        <nuxt-link :to="`/articles/${article.id}`" class="articleCard flex border-4 border-black rounded-lg p-4 bg-white hover:bg-gray-200 items-center justify-between">
+          <h1 class="articleTitle text-lg font-bold break-keep">
             {{ article.title }}
-            </h1>
-          </nuxt-link>
+          </h1>
           <div>
             <img class="articleThumbnail w-20" :src="article.image?.url" alt="Article thumbnail">
           </div>
-        </div>
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -53,3 +51,23 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+.articleTitle {
+  font-size: 20px;
+  width: 600px;
+}
+
+/* メディアクエリを使用してレスポンシブデザインを実現 */
+@media (max-width: 768px) {
+  .articleTitle {
+    width: 190px;
+  }
+}
+
+.articleCard {
+  display: flex;
+  justify-content: space-between; /* タイトルとサムネイルを左右に配置 */
+  align-items: center; /* 中央に揃える */
+}
+</style>
