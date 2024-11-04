68e0fa8235bf390b162237c8dcc6255b437ed74b
Yo Kamada <bumpfuji10@gmail.com>
2024-11-04 23:33:10 +0900

    [improver]header

--- components/header.vue
+++ components/header.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <header class="py-4">
-      <nav class="flex gap-4">
+      <nav class="flex gap-4 items-center">
         <li class="list-none">
           <nuxt-link to="/">
             <img src="/public/img/sphere.png" alt="ブログのロゴ" style="width: 32px">
           </nuxt-link>
         </li>
 
-        <li class="list-none">
+        <li class="list-none p-2 rounded-full bg-gray-300 hover:bg-orange">
           <nuxt-link to="/" class="text-xl font-bold">
             Kyam Blog
           </nuxt-link>
         </li>
 
-        <li class="list-none">
+        <li class="list-none p-2 rounded-full bg-gray-300">
           <nuxt-link to="/about" class="text-xl font-bold">
             about
           </nuxt-link>
