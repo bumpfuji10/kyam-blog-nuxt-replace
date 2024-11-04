9c09fd1dacaf5c7094daf8a94ac2932e55967b1a
Yo Kamada <bumpfuji10@gmail.com>
2024-11-04 17:58:23 +0900

    [improve]記事のサムネが表示されるように

--- pages/home.vue
+++ pages/home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="mx-4">
     <div v-for="(article, index) in articles" :key="index">
-      <div class="py-8 break-words" style="width: 100%;">
-        <nuxt-link :to="`/articles/${article.id}`">
-          <h1 class="mx-auto text-lg font-bold border-4 border-black rounded-lg py-4 bg-white hover:bg-gray-200">
-          {{ article.title }}
-          </h1>
-        </nuxt-link>
+      <div class="break-words article p-4" style="width: 100%;">
+        <div class="articleCard flex border-4 border-black rounded-lg p-4 bg-white hover:bg-gray-200 justify-between items-center">
+          <nuxt-link :to="`/articles/${article.id}`">
+            <h1 class="mx-auto text-lg font-bold break-keep" style="width: 190px;">
+            {{ article.title }}
+            </h1>
+          </nuxt-link>
+          <div>
+            <img class="articleThumbnail w-20" :src="article.image?.url" alt="Article thumbnail">
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,6 +23,9 @@ import { getArticles } from '../src/api/article'
 
 interface Article {
   id: string,
+  image: {
+    url: string
+  }
   title: string,
   content: string,
   createdAt: ""
@@ -37,6 +45,7 @@ export default defineComponent({
       try {
         const response = await getArticles();
         this.articles = response.data.contents;
+        console.log(this.articles[0].image.url)
       } catch (error) {
         console.error('記事の取得中にエラーが発生しました:', error);
       }
